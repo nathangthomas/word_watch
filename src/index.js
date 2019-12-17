e5b6965fdfd4ed0cc4c9c849d2e390c6f300befe
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-17 11:49:37 -0700

    attempt to get post working

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ $(document).ready(() => {
      url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
      success: function(data){
         $(".word-count").append(`The top word is "${Object.keys(data.word)}" and it's count is "${Object.values(data.word)}".`)
-        console.log(Object.values(data.word))
      }
   })
+
+    // $.post('https://wordwatch-api.herokuapp.com/api/v1/words',   // url
+    //    { myData: { "word": { "value": "bahhumbug" } } }, // data to submit
+    //    function(data, status, jqXHR) {// success callback
+    //       $('p').append('status: ' + status + ', data: ' + data);
+    // });
 })
