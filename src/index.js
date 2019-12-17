6ec9da88d7896f8c2fc65d24b352998b2878d009
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-17 11:06:30 -0700

    Add ajax request to render the top word and count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+     url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+     success: function(data){
+        $(".word-count").append(`The top word is "${Object.keys(data.word)}" and it's count is "${Object.values(data.word)}".`)
+        console.log(Object.values(data.word))
+     }
+  })
 })
